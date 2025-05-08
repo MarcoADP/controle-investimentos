75a9116f542f5680af9352739f6db5003715a4a3
Marco Aurélio Deoldoto Paulino <marco.ad.paulino@gmail.com>
2025-05-08 19:54:18 -0300

    Criar Estrutura

---
+++ src/test/java/com/github/marcoadp/controle_investimentos/ControleInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcoadp.controle_investimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
