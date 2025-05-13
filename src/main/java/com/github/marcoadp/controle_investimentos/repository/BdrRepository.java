1b12c963a32113c1974bec770545856398bb3c30
Marco Aurélio Deoldoto Paulino <marco.ad.paulino@gmail.com>
2025-05-13 19:45:33 -0300

    Remover Query desnecessária em BdrRepository

--- src/main/java/com/github/marcoadp/controle_investimentos/repository/BdrRepository.java
+++ src/main/java/com/github/marcoadp/controle_investimentos/repository/BdrRepository.java
@@ -2,7 +2,6 @@ package com.github.marcoadp.controle_investimentos.repository;
 
 import com.github.marcoadp.controle_investimentos.entity.Bdr;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +12,6 @@ public interface BdrRepository extends JpaRepository<Bdr, Long> {
 
     Optional<Bdr> findFirstByCodigo(String codigo);
 
-    @Query("FROM bdr where setor = :setor")
     List<Bdr> findBySetor(String setor);
 
 }
