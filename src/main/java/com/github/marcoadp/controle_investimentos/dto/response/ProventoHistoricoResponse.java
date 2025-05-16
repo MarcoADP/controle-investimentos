6bc9a401c9cd8f34a12812b59e1f6f87b2dff907
Marco Aurélio Deoldoto Paulino <marco.ad.paulino@gmail.com>
2025-05-16 16:57:18 -0300

    Buscar histórico de proventos por ano

---
+++ src/main/java/com/github/marcoadp/controle_investimentos/dto/response/ProventoHistoricoResponse.java
@@ -0,0 +1,7 @@
+package com.github.marcoadp.controle_investimentos.dto.response;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record ProventoHistoricoResponse(BigDecimal valorTotal, List<ProventoAnualResponse> proventos) {
+}
